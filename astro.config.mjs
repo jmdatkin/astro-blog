da932bd336b6811584a1f420ba3635755d7fc2a7
Julian Atkin <jatkindev@gmail.com>
2025-02-12 01:44:52 -0500

    Add OpenAI shield; Delete unnecessary components; Fix site title in Astro config; Fix head tag in layouts;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://jatkin.dev",
   integrations: [mdx(), sitemap()],
 
   vite: {
